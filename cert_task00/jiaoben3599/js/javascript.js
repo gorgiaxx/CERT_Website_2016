42cf3534e3d2116d7b989c8d4eafdc80d4d8d1c3
oMasko <984563783@qq.com>
2016-04-27 21:40:25 +0800

    task

---
+++ cert_task00/jiaoben3599/js/javascript.js
@@ -0,0 +1,130 @@
+
+
+
+function show1(){
+		document.getElementById('name-one').style.display='block';
+
+	};
+	function hide1(){
+		document.getElementById('name-one').style.display='none';
+	};
+	
+	function show2(){
+		document.getElementById('introduce-one').style.display='block';
+
+	};
+	function hide2(){
+		document.getElementById('introduce-one').style.display='none';
+	};
+	
+	function show3(){
+		document.getElementById('our-aim-one').style.display='block';
+
+	};
+	function hide3(){
+		document.getElementById('our-aim-one').style.display='none';
+	};
+
+
+
+function shownumber(){
+			var numbernow = document.getElementById('number-name');
+			
+			document.getElementById('number-name').style.display='block';
+			numbernow.innerHTML="numbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernow<br>numbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernow<br>numbernownumbernownumbernownumbernow<br>numbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernumberninenumber<br>ninenumberninenumberninenumber<br>ninenumberninenumberninenumberninenunnumberninenumberninemberninenumberninenumberninenumberninenumberninenumberninenum<br>berninenumberninenumbernine<br>nunnumberninenumberninemberninenumberninenumberninenumberninenumberninenumberninenumberninenumbernine<br>numberninenunnumberninenumberninemberninenumberninenumberninenumberninenumberninenumberninenumberninenumberninenumberninenunnumberninenumber<br>ninembernine";
+};
+function hidenumber(){
+			document.getElementById('number-name').style.display='none';
+};
+
+
+function shownumbernine(){
+			var numbernow = document.getElementById('number-name');
+			document.getElementById('number-name').style.display='block';			
+			numbernow.innerHTML="numbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernow<br>numbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernow<br>numbernownumbernownumbernownumbernow<br>numbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernumberninenumber<br>ninenumberninenumberninenumber<br>ninenumberninenumberninenumberninenunnumberninenumberninemberninenumberninenumberninenumberninenumberninenumberninenum<br>berninenumberninenumbernine<br>nunnumberninenumberninemberninenumberninenumberninenumberninenumberninenumberninenumberninenumbernine<br>numberninenunnumberninenumberninemberninenumberninenumberninenumberninenumberninenumberninenumberninenumberninenumberninenunnumberninenumber<br>ninembernine";
+};
+function hidenumbernine(){
+			document.getElementById('number-name').style.display='none';
+};
+function shownumbereight(){
+			var numbernow = document.getElementById('number-name');
+			document.getElementById('number-name').style.display='block';			
+			numbernow.innerHTML="numbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernow<br>numbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernow<br>numbernownumbernownumbernownumbernow<br>numbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernumberninenumber<br>ninenumberninenumberninenumber<br>ninenumberninenumberninenumberninenunnumberninenumberninemberninenumberninenumberninenumberninenumberninenumberninenum<br>berninenumberninenumbernine<br>nunnumberninenumberninemberninenumberninenumberninenumberninenumberninenumberninenumberninenumbernine<br>numberninenunnumberninenumberninemberninenumberninenumberninenumberninenumberninenumberninenumberninenumberninenumberninenunnumberninenumber<br>ninembernine";
+};
+function hidenumbereight(){
+			document.getElementById('number-name').style.display='none';
+};
+function shownumberseven(){
+			var numbernow = document.getElementById('number-name');
+			document.getElementById('number-name').style.display='block';			
+			numbernow.innerHTML="numbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernow<br>numbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernow<br>numbernownumbernownumbernownumbernow<br>numbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernumberninenumber<br>ninenumberninenumberninenumber<br>ninenumberninenumberninenumberninenunnumberninenumberninemberninenumberninenumberninenumberninenumberninenumberninenum<br>berninenumberninenumbernine<br>nunnumberninenumberninemberninenumberninenumberninenumberninenumberninenumberninenumberninenumbernine<br>numberninenunnumberninenumberninemberninenumberninenumberninenumberninenumberninenumberninenumberninenumberninenumberninenunnumberninenumber<br>ninembernine";
+};
+function hidenumberseven(){
+			document.getElementById('number-name').style.display='none';
+};
+function shownumbersix(){
+			var numbernow = document.getElementById('number-name');
+			document.getElementById('number-name').style.display='block';			
+			numbernow.innerHTML="numbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernow<br>numbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernow<br>numbernownumbernownumbernownumbernow<br>numbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernumberninenumber<br>ninenumberninenumberninenumber<br>ninenumberninenumberninenumberninenunnumberninenumberninemberninenumberninenumberninenumberninenumberninenumberninenum<br>berninenumberninenumbernine<br>nunnumberninenumberninemberninenumberninenumberninenumberninenumberninenumberninenumberninenumbernine<br>numberninenunnumberninenumberninemberninenumberninenumberninenumberninenumberninenumberninenumberninenumberninenumberninenunnumberninenumber<br>ninembernine";
+};
+function hidenumbersix(){
+			document.getElementById('number-name').style.display='none';
+};
+function shownumberfive(){
+			var numbernow = document.getElementById('number-name');
+			document.getElementById('number-name').style.display='block';			
+			numbernow.innerHTML="numbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernow<br>numbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernow<br>numbernownumbernownumbernownumbernow<br>numbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernumberninenumber<br>ninenumberninenumberninenumber<br>ninenumberninenumberninenumberninenunnumberninenumberninemberninenumberninenumberninenumberninenumberninenumberninenum<br>berninenumberninenumbernine<br>nunnumberninenumberninemberninenumberninenumberninenumberninenumberninenumberninenumberninenumbernine<br>numberninenunnumberninenumberninemberninenumberninenumberninenumberninenumberninenumberninenumberninenumberninenumberninenunnumberninenumber<br>ninembernine";
+};
+function hidenumberfive(){
+			document.getElementById('number-name').style.display='none';
+};
+function shownumberfour(){
+			var numbernow = document.getElementById('number-name');
+			document.getElementById('number-name').style.display='block';			
+			numbernow.innerHTML="numbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernow<br>numbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernow<br>numbernownumbernownumbernownumbernow<br>numbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernumberninenumber<br>ninenumberninenumberninenumber<br>ninenumberninenumberninenumberninenunnumberninenumberninemberninenumberninenumberninenumberninenumberninenumberninenum<br>berninenumberninenumbernine<br>nunnumberninenumberninemberninenumberninenumberninenumberninenumberninenumberninenumberninenumbernine<br>numberninenunnumberninenumberninemberninenumberninenumberninenumberninenumberninenumberninenumberninenumberninenumberninenunnumberninenumber<br>ninembernine";
+};
+function hidenumberfour(){
+			document.getElementById('number-name').style.display='none';
+};
+function shownumberthree(){
+			var numbernow = document.getElementById('number-name');
+			document.getElementById('number-name').style.display='block';			
+			numbernow.innerHTML="numbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernow<br>numbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernow<br>numbernownumbernownumbernownumbernow<br>numbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernumberninenumber<br>ninenumberninenumberninenumber<br>ninenumberninenumberninenumberninenunnumberninenumberninemberninenumberninenumberninenumberninenumberninenumberninenum<br>berninenumberninenumbernine<br>nunnumberninenumberninemberninenumberninenumberninenumberninenumberninenumberninenumberninenumbernine<br>numberninenunnumberninenumberninemberninenumberninenumberninenumberninenumberninenumberninenumberninenumberninenumberninenunnumberninenumber<br>ninembernine";
+};
+function hidenumberthree(){
+			document.getElementById('number-name').style.display='none';
+};
+function shownumbertwo(){
+			var numbernow = document.getElementById('number-name');
+			document.getElementById('number-name').style.display='block';			
+			numbernow.innerHTML="numbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernow<br>numbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernow<br>numbernownumbernownumbernownumbernow<br>numbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernumberninenumber<br>ninenumberninenumberninenumber<br>ninenumberninenumberninenumberninenunnumberninenumberninemberninenumberninenumberninenumberninenumberninenumberninenum<br>berninenumberninenumbernine<br>nunnumberninenumberninemberninenumberninenumberninenumberninenumberninenumberninenumberninenumbernine<br>numberninenunnumberninenumberninemberninenumberninenumberninenumberninenumberninenumberninenumberninenumberninenumberninenunnumberninenumber<br>ninembernine";
+};
+function hidenumbertwo(){
+			document.getElementById('number-name').style.display='none';
+};
+function shownumberone(){
+			var numbernow = document.getElementById('number-name');
+			document.getElementById('number-name').style.display='block';			
+			numbernow.innerHTML="numbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernow<br>numbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernow<br>numbernownumbernownumbernownumbernow<br>numbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernownumbernumberninenumber<br>ninenumberninenumberninenumber<br>ninenumberninenumberninenumberninenunnumberninenumberninemberninenumberninenumberninenumberninenumberninenumberninenum<br>berninenumberninenumbernine<br>nunnumberninenumberninemberninenumberninenumberninenumberninenumberninenumberninenumberninenumbernine<br>numberninenunnumberninenumberninemberninenumberninenumberninenumberninenumberninenumberninenumberninenumberninenumberninenunnumberninenumber<br>ninembernine";
+};
+function hidenumberone(){
+			document.getElementById('number-name').style.display='none';
+};
+
+
+	
+
+	
+
+	
+
+	
+
+	
+
+	
+
+	
+
+
+	
